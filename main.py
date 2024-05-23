f879a7a7ade93ddb25535d33ad64ea1212bf04fd
aaronperezwindows10 <aaronperezwindows10@gmail.com>
2024-05-23 16:08:50 +0000

    ...

--- main.py
+++ main.py
@@ -1,10 +1,113 @@
-# from src.rotate import rotate
-from src.isFull import isFull
-from src.inputFunc import inputFunc
-from src.isValid import is_valid
-from src.recortar_matriz import recortar_matriz
-from src.insertPiece import insertar
 from src.grafica import dibujar_tablero
+
+def isFull(table) -> bool:
+    for i in range(len(table)):
+        for j in range(len(table[0])):
+            if table[i][j] == ".":
+                return False
+    return True
+
+def rotate(M):
+    n = len(M)
+
+    for i in range(n):
+        for j in range(i):
+            temp = M[i][j]
+            M[i][j] = M[j][i]
+            M[j][i] = temp
+
+    for i in range(n):
+        for j in range(n // 2):
+            temp = M[i][j]
+            M[i][j] = M[i][n - j - 1]
+            M[i][n - j - 1] = temp
+
+    return M
+
+def inputFunc():
+    # Ancho, Largo, numero de piezas
+    n, m, p = input().split()
+
+    # Obtener las piezas
+    res = []
+    for _ in range(int(p)):
+        aux = []
+        for _ in range(4):
+            txt = list(input())
+            aux.append(txt)
+        res.append(aux)
+
+    return (res, int(n), int(m), p)
+
+def is_valid(table, piezaR, i, j):
+    # Verificar que las coordenadas (i, j) estén dentro de los límites del tablero
+    if (
+        i < 0
+        or i + len(piezaR) > len(table)
+        or j < 0
+        or j + len(piezaR[0]) > len(table[0])
+    ):
+        return False
+
+    # Verificar si la pieza cabe completamente dentro del tablero
+    for row in range(len(piezaR)):
+        for col in range(len(piezaR[0])):
+            if piezaR[row][col] != ".":
+                if i + row >= len(table) or j + col >= len(table[0]):
+                    return False
+                if table[i + row][j + col] != ".":
+                    return False
+
+    return True
+
+def recortar_matriz(matriz):
+    # Encontrar los límites superior, inferior, izquierdo y derecho
+    fila_superior = None
+    fila_inferior = None
+    columna_izquierda = None
+    columna_derecha = None
+
+    for i, fila in enumerate(matriz):
+        for j, valor in enumerate(fila):
+            if valor != ".":
+                fila_superior = i if fila_superior is None else min(fila_superior, i)
+                fila_inferior = i if fila_inferior is None else max(fila_inferior, i)
+                columna_izquierda = (
+                    j if columna_izquierda is None else min(columna_izquierda, j)
+                )
+                columna_derecha = (
+                    j if columna_derecha is None else max(columna_derecha, j)
+                )
+
+    # Recortar la matriz usando los límites encontrados
+    matriz_recortada = [
+        fila[columna_izquierda : columna_derecha + 1]
+        for fila in matriz[fila_superior : fila_inferior + 1]
+    ]
+    return matriz_recortada
+
+def insertar(table, pieza, i, j):
+    for x in range(len(pieza)):
+        for y in range(len(pieza[0])):
+            if pieza[x][y] != ".":
+                table[i + x][j + y] = pieza[x][y]
+
+    return table
+
+def color(pieza):
+    for i in range(len(pieza)):
+        for j in range(len(pieza[0])):
+            if pieza[i][j] != ".":
+                return pieza[i][j]
+    return "."
+
+# Color: elemento identificar de la pieza
+def quitar(table, color):
+    for i in range(len(table)):
+        for j in range(len(table[0])):
+            if table[i][j] == color:
+                table[i][j] = "."
+    return table
 
 """Este es nuestro main, en donde piezas es una matriz en donde se decriben las piezas, 
 n es el largo de la matriz final, m el ancho de la matriz final
